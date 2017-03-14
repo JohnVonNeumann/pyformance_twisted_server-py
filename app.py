b2c21bdb7fca870c52dc5dcc07a358ab7a45dd59
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-14 21:59:48 +1100

    some basic changes and imports, had a brief hicough with importing the packages from source but realised the errors with a few well placed __init__.py's. added import statements for the pyformance stuff too. also realised just then that when you import a git clone, best off to cd back out of the repo, before you try and push to the wrong repo haha

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from twisted.internet import reactor
 from twisted.web.server import Site
 from twisted.web.resource import Resource
+from pyformance.pyformance.meters import Counter, Histogram, Meter, Timer
+from pyformance.pyformance.registry import MetricsRegistry
 
 class RequestHandler(Resource):
 
+    # can't find much on isLeaf
     isLeaf = True
     def render_GET(self, request):
         request.setResponseCode(200)
